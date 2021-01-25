e30c4833f65c60ac9060fd6cd22ecb556f0545d8
Li Jin <lijin1991.dev@gmail.com>
2021-01-25 10:14:17 +0800

    added big title and description

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React, { ReactElement } from "react";
+import { Row, Col } from "antd";
 
-import "./App.scss";
+import "./styles/App.scss";
 
 function App(): ReactElement {
-  return <div>Landing Page</div>;
+  return (
+    <div className="landing">
+      <Row className="home-section">
+        <Col md={{ span: 10, offset: 2 }}>
+          <div>
+            <span className="big-title">Quadpay anywhere</span>
+          </div>
+          <div>
+            <span className="description">
+              Shop your favorite brands with the Quadpay app—online or in-store.
+            </span>
+          </div>
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default App;
