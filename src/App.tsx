417f5f2fe755c9d75af2da7679a745bddba9aedc
Li Jin <lijin1991.dev@gmail.com>
2021-01-25 11:02:25 +0800

    home section background image added

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React, { ReactElement } from "react";
 import { Row, Col } from "antd";
 
+import appstore from "./assets/images/appstore_icon.png";
+import playstore from "./assets/images/playstore_icon.png";
+
 import "./styles/App.scss";
 
 function App(): ReactElement {
@@ -15,6 +18,16 @@ function App(): ReactElement {
             <span className="description">
               Shop your favorite brands with the Quadpay app—online or in-store.
             </span>
+          </div>
+          <div className="button-wrapper">
+            <div className="store-button">
+              <img src={appstore} alt="appstore" className="appstore-icon" />
+              <span className="store-text">Get on the App Store</span>
+            </div>
+            <div className="store-button">
+              <img src={playstore} alt="playstore" className="appstore-icon" />
+              <span className="store-text">Get on Google Play</span>
+            </div>
           </div>
         </Col>
       </Row>
